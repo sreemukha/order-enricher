56cdac8b76a51e5fd1de19250192fbec8a90c8d9
Sreemukha Taduru <sreemukha.mail@gmail.com>
2025-08-13 01:22:38 -0400

    spring initializr commit

---
+++ src/main/java/com/teamviewer/orderenricher/OrderEnricherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.teamviewer.orderenricher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEnricherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEnricherServiceApplication.class, args);
+	}
+
+}
